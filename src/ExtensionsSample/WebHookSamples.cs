71c44cb0e2a8694f2ffa76fe9f08a3a4891daf6f
Mathew Charles <mathewc@microsoft.com>
2015-08-24 18:30:20 -0700

    Adding WebHook trigger
    Adding WebHooks nuget packaging
    
    Move to use Suave http server
    
    Adding WebHooks end to end tests
    
    Adding WebHook trigger binding extension

---
+++ src/ExtensionsSample/WebHookSamples.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.WebHooks;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace ExtensionsSample
+{
+    public static class WebHookSamples
+    {
+        public static void HookA([WebHookTrigger("sample/hookb")] string body, TraceWriter trace)
+        {
+            trace.Info(string.Format("HookA invoked! Body: {0}", body));
+        }
+
+        public static async Task HookB([WebHookTrigger("sample/hookb")] HttpRequestMessage request, TraceWriter trace)
+        {
+            string body = await request.Content.ReadAsStringAsync();
+            trace.Info(string.Format("HookB invoked! Body: {0}", body));
+        }
+    }
+}
